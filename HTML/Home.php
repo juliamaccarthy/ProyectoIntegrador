11ee0d84113a623463d710a8a71dea9e62a9aa86
Chuli <julia@holaemi.com>
2019-05-16 20:35:58 -0300

    primer commit

--- HTML/Home.php
+++ HTML/Home.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
 
+    <?php include("linkcss.php") ?>
+
+  </head>
+  <body>
+
+    <h1>Escribe conmigo asdjfla</h1>
+    <p>hola euge!</p>
+
+  </body>
+</html>
---
+++ HTML/linkcss.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <!--BOOTSTRAP-->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- TITULOS-->
+    <!-- como se escribe en css
+        font-family: 'Maiden Orange', cursive;-->
+
+    <link href="https://fonts.googleapis.com/css?family=Maiden+Orange" rel="stylesheet">
+
+    <!-- Parrafos-->
+    <!-- como se escribe en el css
+        font-family: 'PT Sans', sans-serif;-->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <!-- NUESTRO CSS -->
+    <link rel="stylesheet" href="../CSS/estilo.css">

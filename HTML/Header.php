68701cbf33842a9ba09aa9e9600b1d8d72a7366f
Chuli <julia@holaemi.com>
2019-05-17 18:09:31 -0300

    header

--- HTML/Header.php
+++ HTML/Header.php
@@ -5,9 +5,12 @@
       <img src="../imagenes/Logo.png" width="30" height="30" class="d-inline-block align-top img-header-logo" alt="">
       Escribe Conmigo
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+
+
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav ml-auto  ul-header">
         <li class="nav-item active">
@@ -17,7 +20,6 @@
           <a class="nav-link" href="#">Temporadas</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Reglas</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -30,14 +32,21 @@
          </div>
        </li>
        <!--LOGIN SINGUP-->
-       <li class="nav-item">
+       <li class="nav-item login">
          <a class="nav-link login" href="#">Log In</a>
        </li>
-       <li class="nav-item">
+       <li class="nav-item singup">
          <a class="nav-link signup" href="#">Sing Up</a>
        </li>
       </ul>
+
+    </ul>
+
+
+
+
       <span class="navbar-text"> </span>
     </div>
+
   </nav>
 </header>
--- HTML/linkcss.php
+++ HTML/linkcss.php
@@ -11,5 +11,13 @@
     <!-- como se escribe en el css
         font-family: 'PT Sans', sans-serif;-->
     <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+
+    <!--FONT AWSOME PARA ICONOS
+        Buscar iconos - https://fontawesome.com/icons?d=gallery&q=instagram
+    -->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
     <!-- NUESTRO CSS -->
     <link rel="stylesheet" href="../CSS/estilo.css">

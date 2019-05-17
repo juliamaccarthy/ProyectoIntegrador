eebc416a7e83492c4221b841ab88c283c18c493f
Chuli <julia@holaemi.com>
2019-05-17 14:28:30 -0300

    header y cambios en el css

--- HTML/Header.php
+++ HTML/Header.php
@@ -1,1 +1,43 @@
-
+<header class="header">
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top header-navbar">
+    <!-- LOGO -->
+    <a class="navbar-brand" href="#">
+      <img src="../imagenes/Logo.png" width="30" height="30" class="d-inline-block align-top img-header-logo" alt="">
+      Escribe Conmigo
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ml-auto  ul-header">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Temporadas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Reglas</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Nosotros
+          </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Quienes Somos</a>
+                <a class="dropdown-item" href="#">Historia</a>
+                <a class="dropdown-item" href="#">Reglas</a>
+         </div>
+       </li>
+       <!--LOGIN SINGUP-->
+       <li class="nav-item">
+         <a class="nav-link login" href="#">Log In</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link signup" href="#">Sing Up</a>
+       </li>
+      </ul>
+      <span class="navbar-text"> </span>
+    </div>
+  </nav>
+</header>

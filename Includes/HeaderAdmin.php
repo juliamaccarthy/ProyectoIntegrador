efd127554b0b405f5012ea37c3c7b40bdeb62a37
Chuli <julia@holaemi.com>
2019-07-30 20:00:50 -0300

    hola

--- Includes/HeaderAdmin.php
+++ Includes/HeaderAdmin.php
@@ -31,10 +31,6 @@
         <li class="nav-item">
           <a class="nav-link" href="escritosAdmin.php">Escritos</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Temporadas</a>
-        </li>
-
         <!-- DROPDOWN QUE NO VAMOSA  USAR AHORA
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">

554fe2f2667a968e5e7a79e10892d5674d94a516
Josefina Berardi <JOSE@Josefinas-MacBook-Air.local>
2019-08-01 20:29:26 -0300

    condiciones

--- Condiciones.php
+++ Condiciones.php
@@ -19,59 +19,78 @@
 
       <h3 class=home-title> Condiciones de Juego  </h3>
       <br>
-      <article class= condiciones_1>
-        <strong> <h5> <class= home-subtitle> ¿Qué hacer si mi compañero no contesta? </h5> </strong>
-      Son tres pasos: <br>
-      1) Revisas el spam de tu correo, a veces nuestro correo es muy sobreprotector con nosotros mismos y nos deja disfrutar de la vida tanto. Debemos pensar que lo hace por nuestro bien. <br>
-      2) Si no encuentras nada en tu spam le pasas un correo a tu compañero. A veces pasa que el correo no se envió, esta excusa es muy creíble y muy real, si se los recuerdas simplemente se sentirán apenados y te lo enviarán y seguramente tomarán lecciones de cómo enviar un simple correo electrónico. <br>
-      3) Si no te contestan avísanos, tenemos métodos de torturas muy pertinentes y seguramente les obligaremos a responderte. En caso que nuestra tortura sea muy fuerte y terminen muertos, igual intentaremos resolverte tu problema de algún modo.
-      <br>
-      <br>
-      </article>
 
       <article class= condiciones_2>
-        <strong> <h5> <class= home-subtitle> ¿Qué hacer si no puedo escribir? </h5> </strong>
-        Si no puedes escribir debiste habernos avisado antes del lunes para sacarte del programa esa semana. Si no lo hiciste asumimos que estás muerto y que, de hecho, no podrías leer esto. Así que no sé para qué lo escribimos. Nuestras condolencias... por ti mismo.
+        <strong> <h5> <class= home-subtitle> ¿Sobre qué escribir? </h5> </strong>
+      Los organizadores proveeran una palabra/frase a forma de disparador de ideas y de historias, pero NO necesariamente debes escribir la palabra, tampoco es necesario que el escrito sea sobre la palabra que ellos mandaron. La idea es <strong> escribir, sin censura de ningun tipo. </strong>.
       <br>
       <br>
       </article>
 
       <article class= condiciones_3>
-        <strong> <h5>  <class= home-subtitle> ¿Puedo invitar gente a esta maravillosa experiencia? </h5> </strong>
-        ¡Claro! Como diríamos en una orgía: ¡mientras más mejor! Danos el correo y nombre de la persona interesada o simplemente dale esta página. Seguramente resolverá el enigma de la inscripción.
+        <strong> <h5>  <class= home-subtitle> ¿Qué sucede cada semana? </h5> </strong>
+          <strong> Los organizadores te enviarán un correo cada semana con: </strong> <br>
+        1) El correo de tu compañero.<br>
+        2) La lista de las palabras y/o frases de la semana. Puedes sugerir palabras y/o frases en el dentro del boletín informativo, siempre serán bienvenidas. <br>
+        <strong>Tú, escribirás a tu compañero cada día de la semana:</strong><br>
+        1) Tu historia.<br>
+        2) La devolución de la historia de tu compañero.
       <br>
       <br>
       </article>
 
       <article class= condiciones_4>
-        <strong> <h5>  <class= home-subtitle> ¿Algún día cobrarán por esto?</h5>  </strong>
-        Esa es la idea, recuerden que este mundo está basado en el sistema monetario y todo esto cuesta esfuerzo, y el esfuerzo se paga con dinero. Igualmente, aún no será. Disfrutemos mientras sea gratis y si nos caes bien seguramente te daremos descuentos.
+        <strong> <h5>  <class= home-subtitle> ¿Cuánto tiempo invertirás?</h5>  </strong>
+      1) 20 minutos en escribir. Si quieres seguir escribiendo luego de que el tiempo acabó puedes hacerlo, pero trata de avisar o sólo envía a tu compañero lo que escribiste dentro del tiempo establecido.
+      Si deseas editar tu escrito, puedes hacerlo.<br>
+      2) Leer el escrito de tu compañero, te llevará máximo de 5 minutos. Y luego, escribir la retroalimentación. En promedio invertirás 30 minutos en completar todo el proceso.
       <br>
       <br>
       </article>
 
       <article class= condiciones_5>
-        <strong> <h5> <class= home-subtitle> ¿Por qué nos dan solamente 20 minutos para escribir? </h5> </strong>
-        El tiempo es una ilusión para diferenciar pasado y futuro. Aunque hicimos un análisis sobre lo prescindible de una persona para escribir y nos dio un resultado que es imposible de explicar... pero da los malditos 20 minutos.
-      <br>
-      <br>
-      </article>
+        <strong> <h5> <class= home-subtitle> ¿Cuándo escribirás?  </h5> </strong>
+  -) Escribirás tres días de la semana: lunes, miércoles y viernes. Toma en cuenta que el día termina a las 12am. NO puedes mandar el escrito del lunes el martes a las 2am. <br>
+  -) Tampoco podrás mandar tres escritos a tu compañero un sólo día.<br>
+  -) Responderás a tu compañero máximo al día siguiente de haberte enviado el escrito. (EJ.:el escrito del lunes, le puedes dar retroalimentación el martes.)<br>
+  -) Si no puedes escribir tu historia por algun motivo, de igual forma dale la devolución a tu compañero. La devolución es uno de los pilares de esta comunidad.<br>
+  Importante: Recuerda que los miembros de Escribe Conmigo se encuentran en todas partes del mundo.
+    <br>
+    <br>
+    </article>
 
-      <article class= condiciones_6>
-        <strong> <h5> <class= home-subtitle> ¿Qué pasa si quiero escribir más?</h5>  </strong>
-         ¡Hazlo! Este proyecto está hecho para inspirarte, solo que no se lo mandes a tu compañero luego de los 20 minutos... o al menos no de manera obligatoria. Al final ustedes pueden enviarse lo que sea.
-      <br>
-      <br>
-      </article>
+    <article class= condiciones_2>
+      <strong> <h5> <class= home-subtitle> ¿Cómo debes mandarlo? </h5> </strong>
+    1) En un Nuevo Correo, envía tu escrito. En el título del correo coloca, únicamente, la palabra/frase del día. En el cuerpo del correo coloca el escrito.No adjuntar ningún archivo.<br>
+    2) Escribe tu retroalimentación haciendo click en el botón “Responder”. Como es un nuevo email, allí pondrás en el cuerpo tus observaciones.
+    <br>
+    <br>
+    </article>
 
-      <article class= condiciones_7>
-        <strong> <h5> <class= home-subtitle> ¿Puedo utilizar otra palabra que no sea la que me dan ustedes? </h5>  </strong>
-      Si. Las palabras solo son una idea, por si acaso no estás inspirado. La verdad puedes escribir sobre lo que te dé la gana y no hay NINGÚN tipo de censura. Puedes escribir sobre lo que hiciste en el día, sobre lo mucho que quieres que se muera una persona, sobre un dolor muy grande, sobre una película (aunque sin spoilers, esto lo decimos por tu propio bien porque nuestros miembros son muy geeks y si les spoileas algo seguramente buscarán la manera de destruirte), sobre lo que quieras.
-      <br>
-      <br>
-      </article>
+    <article class= condiciones_2>
+      <strong> <h5> <class= home-subtitle> ¿Cómo responder a tu compañero? </h5> </strong>
+      No hay nada de malo en querer criticar el escrito de otra persona, pero en Escribe Conmigo lo hacemos de forma constructiva. El objetivo es <strong> escribir </strong> disciplinadamente. Por lo mismo, la retroalimentación debe consistir en enfatizar los aspectos <strong>positivos</strong>. Dile: que te gusta, cuál es tu parte favorita, comenta sobre lo que está allí. Recuerda que Escribe Conmigo fomenta que escribamos, y motivar a tu compañero es parte de nuestra labor como compañeros.
+    <br>
+    <br>
+    </article>
 
-    </div>
+    <article class= condiciones_2>
+      <strong> <h5> <class= home-subtitle> ¿Qué hacer si no puedes escribir? </h5> </strong>
+      Por favor, avisar con antelación si tendrás una semana o un periodo complicado para no tomarte en cuenta. En caso que sea una emergencia, no debes preocuparte, cuando puedas comunicate con los organizadores al siguiente correo electrónico: escribeconmigo.esp@gmail.com
+    <br>
+    <br>
+    </article>
+
+    <article class= condiciones_2>
+      <strong> <h5> <class= home-subtitle> ¿Qué hacer si tu compañero no responde?  </h5> </strong>
+      1) Revisas tu correo SPAM.<br>
+      2) Si allí no está, comunicate con tu compañero para saber qué ha pasado.<br>
+      3) Si no recibes respuesta, comunicate con los organizadores para que puedan asignarte una nueva persona.
+Importante: ¡Contácta a tu compañero lo más rápido posible! No esperes que pasen 3 o 4 días para hacerlo. La idea es mantenernos activos todo el tiempo posible.
+    <br>
+    <br>
+    </article>
+
 <?php include("Includes/Footer.php") ?>
 <?php include("Includes/jquerys.php") ?>
 </body>
